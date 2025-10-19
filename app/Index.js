549808cbf99e0b757b655cde731e0706ffcdb51b
ekalaga <kalagaserge4@gmail.com>
2025-10-19 15:27:29 +0000

    ✨ feat: ajout de la prise en charge de CORS avec des options de configuration

--- app/Index.js
+++ app/Index.js
@@ -1,10 +1,11 @@
 // ============ Import Modules
 const express = require("express");
-const { connect_db } = require("./configs/Database");
 const swaggerUi = require('swagger-ui-express');
+var cors = require('cors')
 // ============ Fin Import Modules
 
 // ============ Import Middlewares
+const { connect_db } = require("./configs/Database");
 const LoginRequired = require("./middlewares/Auth");
 const loggerMiddleware = require("./middlewares/Logger");
 const globalErrorHandler = require("./middlewares/ErrorHandler");
@@ -24,8 +25,15 @@ const PORT = process.env.PORT;
 
 const app = express();
 
+const corsOptions = {
+  origin: '*', // allow all origins for development; restrict in production. Use a specific domain or an array of domains.
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], // specify allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin', 'Access-Control-Allow-Origin'], // specify allowed headers
+  optionsSuccessStatus: 200
+};
 
 // ============ Bloc Middlewares et Configurations
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(updateMetrics);
